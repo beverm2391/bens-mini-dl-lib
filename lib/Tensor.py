00504b07fd2e725c359ce108775d13a741941a52
beverm2391 <beverman4528@gmail.com>
2023-09-07 11:28:39 -0400

    rebuilding tensor class

--- lib/Tensor.py
+++ lib/Tensor.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-class Tensor:
+# ! Old Tensor class, kept for reference
+class TensorV1:
     def __init__(self, data):
         self.data = np.array(data)
         self.shape = self.data.shape
@@ -9,24 +10,24 @@ class Tensor:
 
     # Other refers to the other tensor
     def __add__(self, other):
-        if not isinstance(other, Tensor):
-            return Tensor(self.data + other)
-        return Tensor(self.data + other.data) # element-wise addition
+        if not isinstance(other, TensorV1):
+            return TensorV1(self.data + other)
+        return TensorV1(self.data + other.data) # element-wise addition
 
     def __sub__(self, other):
-        if not isinstance(other, Tensor):
-            return Tensor(self.data - other)
-        return Tensor(self.data - other.data) # element-wise subtraction
+        if not isinstance(other, TensorV1):
+            return TensorV1(self.data - other)
+        return TensorV1(self.data - other.data) # element-wise subtraction
     
     def __mul__(self, other):
-        if not isinstance(other, Tensor):
-            return Tensor(self.data * other)
-        return Tensor(self.data * other.data) # element-wise multiplication
+        if not isinstance(other, TensorV1):
+            return TensorV1(self.data * other)
+        return TensorV1(self.data * other.data) # element-wise multiplication
 
     def __matmul__(self, other): # matrix multiplication
         # Make sure that the other is a tensor
         # This ensures neither are scalar because the Tensor class wont accept scalars
-        if not isinstance(other, Tensor):
+        if not isinstance(other, TensorV1):
             raise TypeError("The 'other' must be an instance of Tensor.")
         
         # check if the last dimension of self is equal to the second last dimension of other (if neither are vectors)
@@ -39,7 +40,7 @@ class Tensor:
         if np.array(result_data).ndim == 0: # if the result is a scalar, return it (because the Tensor class wont accept scalars)
             return result_data
 
-        return Tensor(result_data)
+        return TensorV1(result_data)
 
     @property
     def T(self):
@@ -47,3 +48,108 @@ class Tensor:
     
     def __repr__(self):
         return f"Tensor({self.data.__repr__()})"
+
+# ! Current Tensor class
+class Tensor:
+    def __init__(self, data, requires_grad=True):
+        self.data = np.array(data) # data
+        self.shape = self.data.shape # shape of data
+        self.requires_grad = requires_grad # whether to calculate gradients
+        self.grad = self.zero_grad() if requires_grad else None # gradient of data, if needed
+        self.is_scalar = self.data.ndim == 0 # whether the data is a scalar
+
+    # Constructor Methods =========================================
+    def zero_grad(self):
+        self.grad = np.zeros_like(self.data)
+
+    # Basic Operations ============================================
+    def __add__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return Tensor(self.data + other.data)
+    
+    def __sub__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return Tensor(self.data - other.data)
+
+    def __mul__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return Tensor(self.data * other.data)
+
+    def __truediv__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return Tensor(self.data / other.data)
+    
+    def __matmul__(self, other):
+        if self.is_scalar:
+            return Tensor(self.data * other.data)
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        
+        # v * v
+        if self.data.ndim == 1 and other.data.ndim == 1: # if both are vectors
+            if self.data.shape[0] != other.data.shape[0]: # if the vectors are not the same length
+                raise ValueError(f"Cannot perform matrix multiplication on tensors with shapes {self.data.shape} and {other.data.shape}.")
+
+        # v * m
+        if self.data.ndim == 1 and other.data.ndim > 1:
+            if self.data.shape[0] != other.data.shape[-2]:
+                raise ValueError(f"Cannot perform matrix multiplication on tensors with shapes {self.data.shape} and {other.data.shape}.")
+            
+        # m * v
+        if self.data.ndim > 1 and other.data.ndim == 1:
+            if self.data.shape[-1] != other.data.shape[0]:
+                raise ValueError(f"Cannot perform matrix multiplication on tensors with shapes {self.data.shape} and {other.data.shape}.")
+
+        #  m * m
+        if self.data.ndim > 1 and other.data.ndim > 1:
+            if self.data.shape[-1] != other.data.shape[-2]:
+                raise ValueError(f"Cannot perform matrix multiplication on tensors with shapes {self.data.shape} and {other.data.shape}.")
+
+        result = np.matmul(self.data, other.data)
+        return Tensor(result)
+    
+    # Reduction Operations ========================================
+    def sum(self, axis=None):
+        if self.is_scalar:
+            return self # can't sum a scalar
+        return Tensor(self.data.sum(axis=axis))
+
+    def mean(self, axis=None):
+        if self.is_scalar:
+            return self # can't mean a scalar
+        return Tensor(self.data.mean(axis=axis))
+        
+    def max(self, axis=None):
+        if self.is_scalar:
+            return self # can't max a scalar
+        return Tensor(self.data.max(axis=axis))
+        
+    def min(self, axis=None):
+        if self.is_scalar:
+            return self # can't min a scalar
+        return Tensor(self.data.min(axis=axis))
+    
+    def std(self, axis=None):
+        if self.is_scalar:
+            return self # can't std a scalar
+        return Tensor(self.data.std(axis=axis))
+    
+    # Others ==================================================
+    def reshape(self, *new_shape):
+        return Tensor(self.data.reshape(new_shape))
+    
+    @property
+    def T(self):
+        return np.transpose(self.data)
+
+    def __eq__(self, other):
+        if not isinstance(other, Tensor):
+            return False
+        return np.array_equal(self.data, other.data)
+    
+    def __repr__(self):
+        return f"Tensor({self.data}, requires_grad={self.requires_grad})"
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,75 +1,96 @@
-import pytest
 import numpy as np
+import pytest
+
 from lib.Tensor import Tensor
 
-def test_addition():
-    a = Tensor(np.array([1, 2]))
-    b = Tensor(np.array([3, 4]))
-    result = a + b
-    expected = np.array([4, 6])
-    assert np.all(result.data == expected)
 
-def test_subtraction():
-    a = Tensor(np.array([5, 6]))
-    b = Tensor(np.array([1, 2]))
-    result = a - b
-    expected = np.array([4, 4])
-    assert np.all(result.data == expected)
+#! Basic Tensor Operations ============================================
+def test_scalar_and_scalar():
+    t = Tensor(5)
+    s = 2
+    assert t + s == Tensor(7)
+    assert t - s == Tensor(3)
+    assert t * s == Tensor(10)
+    assert t / s == Tensor(2.5)
 
-def test_multiplication():
-    a = Tensor(np.array([1, 2]))
-    b = Tensor(np.array([3, 4]))
-    result = a * b
-    expected = np.array([3, 8])
-    assert np.all(result.data == expected)
+def test_vector_and_scalar():
+    t = Tensor([1, 2, 3, 4, 5])
+    s = 2
+    assert t + s == Tensor([3, 4, 5, 6, 7])
+    assert t - s == Tensor([-1, 0, 1, 2, 3])
+    assert t * s == Tensor([2, 4, 6, 8, 10])
+    assert t / s == Tensor([0.5, 1, 1.5, 2, 2.5])
+    # assert t @ s == Tensor([2, 4, 6, 8, 10]) # TODO fix matmul
+    
+def test_vector_and_vector():
+    v = Tensor([1, 2, 3, 4, 5])
+    v2 = Tensor([2, 3, 4, 5, 6])
 
-def test_matmul_matrix_matrix():
-    a = Tensor(np.array([[1, 2], [3, 4]]))
-    b = Tensor(np.array([[5, 6], [7, 8]]))
-    result = a @ b
-    expected = np.array([[19, 22], [43, 50]])  # Matrix multiplication
-    assert np.all(result.data == expected)
+    assert v + v2 == Tensor([3, 5, 7, 9, 11])
+    assert v - v2 == Tensor([-1, -1, -1, -1, -1])
+    assert v * v2 == Tensor([2, 6, 12, 20, 30])
+    assert v / v2 == Tensor([0.5, 2/3, 3/4, 4/5, 5/6])
+    assert v @ v2 == Tensor(70)
 
-def test_matmul_matrix_vector():
-    a = Tensor(np.array([[1, 2], [3, 4]]))
-    b = Tensor(np.array([5, 6]))
-    result = a @ b
-    expected = np.array([17, 39])  # [1*5 + 2*6, 3*5 + 4*6]
-    assert np.all(result.data == expected)
+def test_vector_and_tensor():
+    v = Tensor([1, 2, 3])
+    v2 = Tensor([1, 2, 3, 4])
+    t = Tensor([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
 
-def test_matmul_vector_vector():
-    a = Tensor(np.array([1, 2]))
-    b = Tensor(np.array([3, 4]))
-    result = a @ b
-    expected = 11  # 1*3 + 2*4
-    assert result == expected
+    assert v @ t == Tensor([30, 36, 42])
 
-def test_matmul_dimension_mismatch():
-    a = Tensor(np.array([[1, 2], [3, 4], [5, 6]]))
-    b = Tensor(np.array([[5, 6], [7, 8], [9, 10]]))
     with pytest.raises(ValueError):
-        a @ b
+        v2 @ t
 
-def test_transpose():
-    a = Tensor(np.array([[1, 2], [3, 4]]))
-    result = a.T
-    expected = np.array([[1, 3], [2, 4]])
-    assert np.all(result == expected)
+def test_tensor_and_tensor():
+    t = Tensor([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
 
-def test_addition_with_scalar():
-    a = Tensor(np.array([1, 2]))
-    result = a + 2
-    expected = np.array([3, 4])
-    assert np.all(result.data == expected)
+    t2 = Tensor([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
 
-def test_subtraction_with_scalar():
-    a = Tensor(np.array([5, 6]))
-    result = a - 1
-    expected = np.array([4, 5])
-    assert np.all(result.data == expected)
+    t3 = Tensor([
+        [1, 2, 3],
+        [4, 5, 6]
+    ])
 
-def test_multiplication_with_scalar():
-    a = Tensor(np.array([1, 2]))
-    result = a * 3
-    expected = np.array([3, 6])
-    assert np.all(result.data == expected)
+    assert t @ t2 == Tensor([
+        [30, 36, 42],
+        [66, 81, 96],
+        [102, 126, 150]
+    ])
+
+    with pytest.raises(ValueError):
+        t @ t3
+
+def test_all_basic_methods():
+    test_scalar_and_scalar()
+    test_vector_and_scalar()
+    test_vector_and_vector()
+    test_vector_and_tensor()
+    test_tensor_and_tensor()
+
+#! Reduction Operations ========================================
+# TODO: test reduction operations
+
+# ! Shape Operations ========================================
+# TODO: test shape operations
+
+# ! Indexing Operations ========================================
+# TODO: test indexing operations
+
+# ! Utility Methods ========================================
+# TODO: test utility methods
+
+# ! Other Methods ========================================
+# TODO: test other methods
