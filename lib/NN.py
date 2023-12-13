fixed the CCELoss and added two new activations, log softmax and negative log liklehood

lib/NN.py
@@ -109,6 +109,45 @@ class LeakyReLU(Module):
 
         return out
 
+# TODO - needs individual test, but tested as part of test_CategoricalCrossEntropyLoss
+class LogSoftmax(Module):
+    @force_tensor_method
+    def forward(self, x: Tensor) -> Tensor:
+        # Apply the log-softmax function
+        e_x = np.exp(x.data - np.max(x.data, axis=1, keepdims=True))  # for numerical stability
+        out_data = np.log(e_x / e_x.sum(axis=1, keepdims=True))
+        out = Tensor(out_data, (x,), 'LogSoftmax', requires_grad=x.requires_grad)
+
+        def _backward():
+            # Gradient of log-softmax is the output gradient minus the exp of output times sum of output gradients
+            softmax = np.exp(out_data)
+            x.grad += out.grad - softmax * np.sum(out.grad, axis=1, keepdims=True)
+        out._backward = _backward
+
+        return out
+
+# TODO - needs individual test, but tested as part of test_CategoricalCrossEntropyLoss
+class NegativeLogLikelihoodLoss(Module):
+    @force_tensor_method
+    def forward(self, x: Tensor, y: Tensor) -> Tensor:
+        # x is expected to be log probabilities (output of LogSoftmax)
+        # y is expected to be class indices
+        batch_size = x.shape[0]
+        # Select the log probabilities of the correct classes
+        correct_log_probs = x.data[np.arange(batch_size), y.data.astype(int)]
+        # Compute the negative log likelihood
+        out_data = -correct_log_probs.mean()
+        out = Tensor(out_data, (x, y), 'NLLLoss', requires_grad=x.requires_grad)
+
+        def _backward():
+            # The gradient is -1 at the indices of the correct classes, divided by the batch size
+            grad = np.zeros_like(x.data)
+            grad[np.arange(batch_size), y.data.astype(int)] = -1 / batch_size
+            x.grad += grad * out.grad
+        out._backward = _backward
+
+        return out
+
 # ! Loss Functions =======================================================
 
 class Loss(Module):
@@ -129,14 +168,19 @@ class BinaryCrossEntropyLoss(Loss):
         ce = - (y * x.log() + (1. - y) * (1. - x).log()).mean() # cross entropy
         return ce
 
-
 class CategoricalCrossEntropyLoss(Loss):
+    def __init__(self):
+        super().__init__()
+        self.log_softmax = LogSoftmax()
+        self.nll_loss = NegativeLogLikelihoodLoss()
+
+    @force_tensor_method
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
-        self.input = (x, y)
-        epsilon = 1e-12
-        x = x.clip(epsilon, 1. - epsilon) # clip to avoid log(0)
-        ce = - (y * x.log()).sum(axis=1).mean() # cross entropy
-        return ce
+        # this takes in class labels instead of one-hot vectors, just like pytorch
+        self.input = (x, y) # save input
+        log_probs = self.log_softmax.forward(x) # Apply log softmax to the input tensor x
+        loss = self.nll_loss.forward(log_probs, y) # Compute the negative log likelihood loss
+        return loss
 
 # ! Layers ===============================================================
 

lib/Tensor.py
@@ -411,8 +411,9 @@ class Tensor:
 
             # Case 2 & 3: General tensor (with or without axis)
             else:
-                expanded_grad = np.ones_like(self.data) * out.grad
-                if axis is not None:
+                if axis is None:
+                    expanded_grad = np.ones_like(self.data) * out.grad
+                else: # axis is not none
                     # reshape or expand 
                     expanded_grad = np.expand_dims(out.grad, axis=axis)
                 self.grad += expanded_grad

tests/test_loss.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 
 from lib.Tensor import Tensor
-from lib.NN import MSELoss, CrossEntropyLoss
+from lib.NN import MSELoss, BinaryCrossEntropyLoss, CategoricalCrossEntropyLoss
 
 def test_MSE():
     x_data = np.array([0.1, 0.2, 0.3, 0.4])
@@ -27,17 +27,14 @@ def test_MSE():
     assert np.allclose(x.grad, x_torch.grad.numpy()), f"x.grad: {x.grad}\nx_torch.grad: {x_torch.grad.numpy()}"
     assert np.allclose(y.grad, y_torch.grad.numpy()), f"y.grad: {y.grad}\ny_torch.grad: {y_torch.grad.numpy()}"
 
-def test_CrossEntropyLoss():
+def test_BinaryCrossEntropyLoss():
     x_data = np.array([0.1, 0.8, 0.4, 0.6])
     y_data = np.array([0, 1, 0, 1])
-
-    x_data = np.random.rand(10, 5)
-    y_data = np.random.rand(10, 5)
 
     x = Tensor(x_data, requires_grad=True)
     y = Tensor(y_data, requires_grad=True)
 
-    criterion = CrossEntropyLoss()
+    criterion = BinaryCrossEntropyLoss()
     loss = criterion(x, y)
     loss.backward()
 
@@ -52,3 +49,25 @@ def test_CrossEntropyLoss():
     assert np.allclose(loss.data, loss_torch.data.numpy()), f"loss.data: {loss.data}\nloss_torch.data: {loss_torch.data.numpy()}"
     assert np.allclose(x.grad, x_torch.grad.numpy()), f"x.grad: {x.grad}\nx_torch.grad: {x_torch.grad.numpy()}"
     assert np.allclose(y.grad, y_torch.grad.numpy()), f"y.grad: {y.grad}\ny_torch.grad: {y_torch.grad.numpy()}"
+
+def test_CategoricalCrossEntropyLoss():
+    x_data = np.array([[0.1, 0.6, 0.3], [0.8, 0.1, 0.1], [0.3, 0.1, 0.6], [0.2, 0.2, 0.6]]) # prob distributions
+    y_data = np.array([1, 0, 2, 2])
+
+    x = Tensor(x_data, requires_grad=True)
+    y = Tensor(y_data, requires_grad=False)
+
+    criterion = CategoricalCrossEntropyLoss()
+    loss = criterion(x, y)
+    loss.backward()
+
+    # Using PyTorch's built-in BCELoss
+    x_torch = torch.tensor(x_data, dtype=torch.float32, requires_grad=True)
+    y_torch = torch.tensor(y_data, dtype=torch.long, requires_grad=False)
+
+    criterion_torch = torch.nn.CrossEntropyLoss()
+    loss_torch = criterion_torch(x_torch, y_torch)
+    loss_torch.backward()
+
+    assert np.allclose(loss.data, loss_torch.data.numpy()), f"loss.data: {loss.data}\nloss_torch.data: {loss_torch.data.numpy()}"
+    assert np.allclose(x.grad, x_torch.grad.numpy()), f"x.grad: {x.grad}\nx_torch.grad: {x_torch.grad.numpy()}"